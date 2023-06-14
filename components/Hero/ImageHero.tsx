182d11dd0e731f648251671436d5f549b09f5f05
stephane.malho <stepahen.malho@gmail.com>
2023-06-14 10:32:20 +0200

    ♻️  F01: add Hero folder & create heros components

---
+++ components/Hero/ImageHero.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const ImageHero = () => {
+  return (
+    <div className="hero__image-container">
+    <div className="hero__image">
+      <Image src="/hero.png" alt="hero" fill className="object-contain "/>
+      <div className="hero__image-overlay"/>
+    </div>
+  </div>
+  )
+}
+
+export default ImageHero
